5038ab1eb9fb01430cd71f3bd59364798e234231
Isha Negi <negi.isha98@gmail.com>
2025-04-04 18:49:39 +0530

    h-full login, signup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -33,13 +33,13 @@ function Login() {
         </div>
       </div>
 
-      <div className=" w-full sm:w-1/2 h-screen fixed right-0 top-0 sm:rounded-s-3xl rounded-none sm:bg-[#FFFFFF] sm:text-black bg-[#133221] text-[#FFFFFF] flex items-center justify-center">
+      <div className=" w-full sm:w-1/2 h-full fixed right-0 top-0 sm:rounded-s-3xl rounded-none sm:bg-[#FFFFFF] sm:text-black bg-[#133221] text-[#FFFFFF] flex items-center justify-center">
         <div className=" sm:w-[70%] mx-1">
-          <p className=" font-Telex sm:text-2xl lg:text-3xl text-3xl font-medium lg:mb-7 mb-7 sm:mb-4">
+          <p className=" font-Telex sm:text-2xl lg:text-4xl text-4xl font-medium lg:mb-7 mb-7 sm:mb-4">
             Welcome Back
           </p>
           <form className="flex flex-col font-Telex" onSubmit={handleSubmit}>
-            <label htmlFor="email" className="mb-2 sm:text-md lg:text-lg text-lg">
+            <label htmlFor="email" className="mb-2 sm:text-md lg:text-xl text-xl">
               Email Address
             </label>
             <input
@@ -51,7 +51,7 @@ function Login() {
               className="border-0 border-b-2 border-[#FFFFFF] py-1 px-2 rounded-md lg:mb-6 sm:mb-4 mb-6 bg-transparent h-7 sm:border-[#133221]"
             />
 
-            <label htmlFor="password" className="sm:text-md text-lg mb-2">
+            <label htmlFor="password" className="sm:text-md text-xl lg:text-xl mb-2">
               Password
             </label>
             <input
@@ -65,7 +65,7 @@ function Login() {
             {/* <p className=" text-red text-lg">{error}</p> */}
             <button
               type="submit"
-              className=" bg-main sm:text-white sm:bg-[#133221] text-[#FFFFFF] bg-[#fa453c] rounded-lg sm:w-52 lg:w-60 w-60 sm:text-md lg:text-lg text-lg font-medium transition-all duration-500 ease-linear transform hover:scale-110 font-Coustard py-1 mt-6 mx-auto"
+              className=" bg-main sm:text-white sm:bg-[#133221] text-[#FFFFFF] bg-[#fa453c] rounded-lg sm:w-52 lg:w-64 w-64 sm:text-md lg:text-xl text-xl font-medium transition-all duration-500 ease-linear transform hover:scale-110 font-Coustard py-1 mt-6 mx-auto"
             >
               Login
             </button>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -36,16 +36,16 @@ function Signup() {
         </div>
       </div>
 
-      <div className=" w-full sm:w-1/2 h-screen fixed right-0 top-0 sm:rounded-s-3xl rounded-none sm:bg-[#FFFFFF] sm:text-black bg-[#133221] text-[#FFFFFF] flex items-center justify-center">
+      <div className=" w-full sm:w-1/2 h-full fixed right-0 top-0 sm:rounded-s-3xl rounded-none sm:bg-[#FFFFFF] sm:text-black bg-[#133221] text-[#FFFFFF] flex items-center justify-center">
       <div className=" sm:w-[70%] mx-1">
-        <p className=" font-Telex sm:text-2xl lg:text-3xl text-3xl font-medium lg:mb-7 mb-7 sm:mb-4">
+        <p className=" font-Telex sm:text-2xl lg:text-4xl text-4xl font-medium lg:mb-7 mb-7 sm:mb-4">
           Create Account
         </p>
         <form
           className="flex flex-col font-Telex"
           onSubmit={handleSubmit}
         >
-          <label htmlFor="fullname" className="mb-2 sm:text-md lg:text-lg text-lg">
+          <label htmlFor="fullname" className="mb-2 sm:text-md lg:text-xl text-xl">
             Full Name
           </label>
           <input
@@ -56,7 +56,7 @@ function Signup() {
             required
             className="border-0 border-b-2 border-[#FFFFFF] py-1 px-2 rounded-md lg:mb-6 sm:mb-4 mb-6 bg-transparent h-7 sm:border-[#133221]"
           />
-          <label htmlFor="email" className=" sm:text-md text-lg mb-2">
+          <label htmlFor="email" className=" sm:text-md lg:text-xl text-xl mb-2">
             Email Address
           </label>
           <input
@@ -68,7 +68,7 @@ function Signup() {
             className="border-0 border-b-2 border-[#FFFFFF] py-1 px-2 rounded-md lg:mb-6 sm:mb-4 mb-6 bg-transparent h-7 sm:border-[#133221]"
           />
 
-          <label htmlFor="password" className=" sm:text-md text-lg mb-2">
+          <label htmlFor="password" className=" sm:text-md lg:text-xl text-xl mb-2">
             Password
           </label>
           <input
@@ -82,7 +82,7 @@ function Signup() {
           {/* <p className=" text-red text-lg">{error}</p> */}
           <button
             type="submit"
-            className=" bg-main sm:text-white sm:bg-[#133221] text-[#FFFFFF] bg-[#fa453c] rounded-lg sm:w-52 lg:w-60 w-60 sm:text-md lg:text-lg text-lg font-medium transition-all duration-500 ease-linear transform hover:scale-110 font-Coustard py-1 mt-6 mx-auto"
+            className=" bg-main sm:text-white sm:bg-[#133221] text-[#FFFFFF] bg-[#fa453c] rounded-lg sm:w-52 lg:w-64 w-64 sm:text-md lg:text-xl text-xl font-medium transition-all duration-500 ease-linear transform hover:scale-110 font-Coustard py-1 mt-6 mx-auto"
           >
             Create Account
           </button>
